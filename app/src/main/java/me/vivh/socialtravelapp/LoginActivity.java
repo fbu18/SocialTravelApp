c6e30df2b63d843f48eb26c97d5ec1170c71233b
Kathy Huang <kathyhuang@fb.com>
2018-07-16 15:29:16 -0700

    Added login capabilities

--- app/src/main/java/me/vivh/socialtravelapp/LoginActivity.java
+++ app/src/main/java/me/vivh/socialtravelapp/LoginActivity.java
@@ -1,13 +1,70 @@
 package me.vivh.socialtravelapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.etUsername) EditText etUsername;
+
+    @BindView(R.id.etPassword) EditText etPassword;
+
+    @BindView(R.id.btnLogin) Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if(currentUser != null){
+            final Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+                login(username,password);
+            }
+        });
+
+
+    }
+
+    private void login(String username, String password){
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e == null){
+                    Log.d("LoginActivity", "Login successful.");
+                    final Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Log.d("LoginActivity", "Login failure.");
+                    Toast.makeText(LoginActivity.this, "Login failed.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
