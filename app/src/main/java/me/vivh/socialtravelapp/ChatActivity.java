e8b35e7b3c6ec5290bbb6a24115d54fd34e98c52
Kathy Huang <kathyhuang@fb.com>
2018-08-06 16:06:40 -0700

    Fixed launch screen continually dispalying in background issue

--- app/src/main/java/me/vivh/socialtravelapp/ChatActivity.java
+++ app/src/main/java/me/vivh/socialtravelapp/ChatActivity.java
@@ -68,6 +68,7 @@ public class ChatActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
