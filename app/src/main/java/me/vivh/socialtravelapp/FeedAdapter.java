ec6d76a997c1b507cc192d45aada60d798cd2731
David Mindlin <dmindlin@fb.com>
2018-08-06 18:02:55 -0700

    made the loaded maps look better, gave up and used static maps instead of dynamic ones

--- app/src/main/java/me/vivh/socialtravelapp/FeedAdapter.java
+++ app/src/main/java/me/vivh/socialtravelapp/FeedAdapter.java
@@ -210,7 +210,7 @@ public class FeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
             String lat = Double.toString(point.getLatitude());
             String lng = Double.toString(point.getLongitude());
             // Use Google Static Maps API to get an image of the map surrounding the attraction
-            String mapUrl = "http://maps.google.com/maps/api/staticmap?center=" + lat + "," + lng + "&zoom=15&scale=1&size=200x200&maptype=roadmap&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:%7C" + lat + "," + lng;
+            String mapUrl = "http://maps.google.com/maps/api/staticmap?center=" + lat + "," + lng + "&zoom=15&scale=2&size=1000x2000&maptype=roadmap&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:%7C" + lat + "," + lng;
             Glide.with(context).load(mapUrl).apply(RequestOptions.placeholderOf(R.drawable.background_gradient)).into(viewHolderCheckIn.ivMap);
             return;
         }
